96eedd43ec1d6f57032a637e56dc88c29767d443
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-07-24 17:06:50 -0300

    funcionando

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -17,11 +17,11 @@ namespace Epic_Arts_Entertainment.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Cadastro()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- obj/Debug/net8.0/Epic Arts Entertainment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Epic Arts Entertainment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f424fcac2344564fbaa70c8c9a94040979ceb1bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b541d8a14a39d708f832a66ea7714553a4f9a27f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
