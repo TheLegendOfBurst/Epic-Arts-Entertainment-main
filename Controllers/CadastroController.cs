d716af7e6467a162c012b8765d3d998b7f5d3ab2
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-07-24 15:52:50 -0300

    s

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,32 @@
+using Epic_Arts_Entertainment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Epic_Arts_Entertainment.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private readonly ILogger<CadastroController> _logger;
+
+        public CadastroController(ILogger<CadastroController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
