20498ec4fa3b9c228af6cc4449c3ba25ca541e3f
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-07-24 14:36:13 -0300

    123

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Epic_Arts_Entertainment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
