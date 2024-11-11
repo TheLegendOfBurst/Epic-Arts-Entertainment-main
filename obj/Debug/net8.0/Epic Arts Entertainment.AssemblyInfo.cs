2676b34ceab5e7bd70d9c3281a29f43c99cc9879
Scoob26 <cantoaraatila@gmail.com>
2024-11-11 14:46:57 -0300

    arrumei submenu

--- obj/Debug/net8.0/Epic Arts Entertainment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Epic Arts Entertainment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c11ea80124321321bd7934117eec76f5da10f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efda174153be7cda974529bca8b2ab45d0851fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
