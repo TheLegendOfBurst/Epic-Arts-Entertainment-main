39c8ce408e8ea3df07528dcfd5f094bda9971100
TheLegendOfBurst <141189394+TheLegendOfBurst@users.noreply.github.com>
2025-02-04 17:46:42 -0300

    Relatorio

--- obj/Debug/net8.0/Epic Arts Entertainment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Epic Arts Entertainment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f7af12760976e331fa84cfb2cd08456a4c5c6f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bda13dd686328b48087ea7c74fd046a2a73af4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Epic Arts Entertainment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
